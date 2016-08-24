e9b92b301379944be4c8244c8211dbebe2b59a21
Santos Gallegos <santos_g@outlook.com>
2016-08-23 21:50:41 -0500

    Testcase added

--- tests/test_isbn_from_pdf.py
+++ tests/test_isbn_from_pdf.py
@@ -1,7 +1,7 @@
 import unittest
 
-from bookpy.bookpy import get_isbn_from_pdf
-from bookpy.errors import ISBNNotFoundError
+from bookpy import get_isbn_from_pdf
+from bookpy import ISBNNotFoundError
 
 
 class TestGetISBNFromPDF(unittest.TestCase):
@@ -14,6 +14,11 @@ class TestGetISBNFromPDF(unittest.TestCase):
     def test_isbn_soft_skills_pdf(self):
         pdf_file = "pdf/soft_skills.pdf"
         isbns = ('1617292397', '9781617292392')
+        self.assertIn(get_isbn_from_pdf(pdf_file), isbns)
+
+    def test_isbn_introduction2algorithms(self):
+        pdf_file = "pdf/introduction2algorithms.pdf"
+        isbns = ('9780262033848', '0262033844')
         self.assertIn(get_isbn_from_pdf(pdf_file), isbns)
 
     def test_pdf_without_text(self):
