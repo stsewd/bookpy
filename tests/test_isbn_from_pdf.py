b84f07c503039b579e6285b018973736a94da946
Santos Gallegos <santos_g@outlook.com>
2016-08-27 19:01:17 -0500

    update test

--- tests/test_isbn_from_pdf.py
+++ tests/test_isbn_from_pdf.py
@@ -7,16 +7,16 @@ from bookpy import ISBNNotFoundError
 class TestGetISBNFromPDF(unittest.TestCase):
 
     def test_gnu_make(self):
-        pdf_file = "test_resources/gnu_make.pdf"
+        pdf_file = "tests_resources/gnu_make.pdf"
         isbns = ('0596006101', '9780596006105')
         self.assertIn(get_isbn_from_pdf(pdf_file), isbns)
 
     def test_pdf_without_isbn(self):
-        pdf_file = "test_resources/no_isbn.pdf"
+        pdf_file = "tests_resources/no_isbn.pdf"
         with self.assertRaises(ISBNNotFoundError):
             get_isbn_from_pdf(pdf_file)
 
     def test_no_pdf(self):
-        file_ = "test_resources/no_pdf.png"
+        file_ = "tests_resources/no_pdf.png"
         with self.assertRaises(ISBNNotFoundError):
             get_isbn_from_pdf(file_)
--- tests/test_rename_files.py
+++ tests/test_rename_files.py
@@ -7,7 +7,7 @@ from bookpy import rename_files
 class TestRenameFiles(unittest.TestCase):
 
     def test_gnu_make(self):
-        file_path = "test_resources/gnu_make.pdf"
-        new_file_path = "test_resources/Managing Projects With GNU Make - Robert Mecklenburg (2004).pdf"
+        file_path = "tests_resources/gnu_make.pdf"
+        new_file_path = "tests_resources/Managing Projects With GNU Make - Robert Mecklenburg (2004).pdf"
         rename_files([file_path])
         self.assertTrue(path.exists(new_file_path))
